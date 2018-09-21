e5b41a8ff3de4264273cbe2df64e1e6db83d2d7a
Mohammad Rebati <m.rebati@icloud.com>
2018-09-21 16:54:43 +0430

    add infrastructure for application core
    fixed issue #9

---
+++ Eron.mailClient.Application.Core/Infrastructure/Entity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Eron.MailClient.Application.Core.Infrastructure
+{
+    public class Entity<TPrimaryKey> : IEntity
+    {
+        public Entity()
+        {
+            this.CreateDateTime = DateTime.Now;
+        }
+
+        public TPrimaryKey Id { get; set; }
+
+        public string CreatorUserId { get; set; }
+
+        public string UpdateUserId { get; set; }
+
+        public DateTime UpdateDateTime { get; set; }
+
+        public DateTime CreateDateTime { get; private set; }
+
+        public bool IsDeleted { get; set; }
+
+        object IEntity.Id
+        {
+            get
+            {
+                return this.Id;
+            }
+            set
+            {
+                if (value is TPrimaryKey)
+                    this.Id = (TPrimaryKey)value;
+            }
+        }
+    }
+}
