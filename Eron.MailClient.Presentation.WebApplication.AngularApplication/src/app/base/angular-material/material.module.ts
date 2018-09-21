460f484f2cbc242311a5eadc1137f17ac00780cb
Mohammad Rebati <m.rebati@icloud.com>
2018-09-21 16:11:45 +0430

    add user menu
    #1 done

--- Eron.MailClient.Presentation.WebApplication.AngularApplication/src/app/base/angular-material/material.module.ts
+++ Eron.MailClient.Presentation.WebApplication.AngularApplication/src/app/base/angular-material/material.module.ts
@@ -9,7 +9,8 @@ import {
   MatExpansionModule,
   MatSidenavModule,
   MatSnackBarModule,
-  MatListModule
+  MatListModule,
+  MatMenuModule
 } from '@angular/material';
 
 @NgModule({
@@ -24,7 +25,8 @@ import {
     MatExpansionModule,
     MatSidenavModule,
     MatSnackBarModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule
   ],
   exports: [
     MatToolbarModule,
@@ -36,7 +38,8 @@ import {
     MatExpansionModule,
     MatSidenavModule,
     MatSnackBarModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule {}
